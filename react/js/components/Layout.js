4eb539982cb28b610c7b878c9ecfa5cfb197a0e7
KevFan <kevin_fan@hotmail.co.uk>
2017-09-23 14:08:23 +0100

    Add superagent to call api endpoint + test for getting list of users using it

--- react/js/components/Layout.js
+++ react/js/components/Layout.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import SuperAgent from 'superagent'; // small progressive client-side HTTP request library
 
 import Header from './Header';
 
 export default class Layout extends React.Component {
+  /**
+   * React method that is invoked immediately after a component is mounted
+   * Uses SuperAgent to call /users endpoint to get list of users
+   */
+  componentDidMount() {
+    SuperAgent
+      .get('/users')
+      .end((error, response) => {
+        if (error) {
+          console.log('Error getting list of users from Layout');
+        } else {
+          console.log(response.body);
+        }
+      });
+  }
+
   render() {
     return (
       <div>
